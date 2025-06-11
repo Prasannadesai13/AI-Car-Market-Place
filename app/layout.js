417d78315e3f9c949e7dec36f4b406d0f9ecb108
Prasanna desai <prasannadesai940@gmail.com>
2025-06-11 20:44:46 +0530

    second commit

--- app/layout.js
+++ app/layout.js
@@ -18,14 +18,13 @@ export default function RootLayout({ children }) {
         <head>
           <link rel="icon" href="/logo-white.png" sizes="any" />
         </head>
-        <body className={`${inter.className}`}>
+        <body className={inter.className}>
           <Header />
           <main className="min-h-screen">{children}</main>
           <Toaster richColors />
-
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with 💗 by RoadsideCoder</p>
+              <p>Made with 💗 by Car guys</p>
             </div>
           </footer>
         </body>
