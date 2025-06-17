6bb41b8b03c6db387d4f2598b08980a332f9021a
Prasanna desai <prasannadesai940@gmail.com>
2025-06-17 20:23:44 +0530

    fourth  commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        serverComponentsHmrCache: false, // defaults to true
+        serverComponentsHmrCache: false,
+        nodeMiddleware: true, // defaults to true
     },
 
     images: {
